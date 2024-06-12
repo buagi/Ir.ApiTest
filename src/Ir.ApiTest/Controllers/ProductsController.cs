a4fb72697071e96a1a0767cb32255ef0b4d50b4c
Blazej <blazejur@gmail.com>
2024-06-12 18:16:58 +0100

    Initialized tests projects and added basic test for create product.

--- src/Ir.ApiTest/Controllers/ProductsController.cs
+++ src/Ir.ApiTest/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
 using Ir.IntegrationTest.Contracts;
+using Ir.IntegrationTest.Entity;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ir.FakeMarketplace.Controllers;
 
@@ -8,6 +10,7 @@ namespace Ir.FakeMarketplace.Controllers;
 [Route("api/[controller]")]
 public class ProductsController : ControllerBase
 {
+
   [HttpGet]
   public IEnumerable<Product> GetProducts()
   {
@@ -21,9 +24,21 @@ public class ProductsController : ControllerBase
   }
 
   [HttpPost()]
-  public IActionResult CreateProduct([FromBody] Product product)
+  public async Task<IActionResult> CreateProduct([FromBody] Product product, Context dbContext)
   {
-    throw new NotImplementedException();
+    if (await dbContext.Products.AnyAsync(p => p.Id == product.Id))
+    {
+      return Conflict();
+    }
+
+    product.Created = DateTimeOffset.UtcNow;
+    product.LastUpdated = DateTimeOffset.UtcNow;
+
+    //ToDo: Add separate validators namespace / project
+    //dbContext.Products.Add(product);
+    await dbContext.SaveChangesAsync();
+
+    return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
   }
 
   [HttpPatch("{id}")]
